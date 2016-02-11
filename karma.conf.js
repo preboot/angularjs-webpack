9d98703eb57b7a265d424621aede3893065b7297
Foxandxss <foxandxss@gmail.com>
2016-02-11 18:34:57 +0100

    swap karma launcher for phantomjs2

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function karmaConfig (config) {
     },
 
     browsers: [
-      // Run tests using PhantomJS
-      'PhantomJS'
+      // Run tests using PhantomJS2
+      'PhantomJS2'
     ],
 
     singleRun: true,
