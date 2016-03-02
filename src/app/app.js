0499f98410599fa697daa5a34cc057dfbfe29adf
Foxandxss <foxandxss@gmail.com>
2016-03-02 13:21:34 +0100

    add a minimal application with tests

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,25 @@
 import angular from 'angular';
 
-angular.module('app', []);
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
---
+++ src/app/app.spec.js
@@ -0,0 +1,20 @@
+import app from './app';
+
+describe('app', () => {
+
+  describe('AppCtrl', () => {
+    let ctrl;
+
+    beforeEach(() => {
+      angular.mock.module(app);
+
+      angular.mock.inject(($controller) => {
+        ctrl = $controller('AppCtrl', {});
+      });
+    });
+
+    it('should contain the starter url', () => {
+      expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
+    });
+  });
+});
