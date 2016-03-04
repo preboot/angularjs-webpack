325fe0a55ceb0fa4ca0dfb6a8fe3f6bd8e8ee66a
Foxandxss <foxandxss@gmail.com>
2016-03-04 11:38:13 +0100

    move back to phantomjs launcher
    Fixes #17

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function karmaConfig (config) {
     },
 
     browsers: [
-      // Run tests using PhantomJS2
-      'PhantomJS2'
+      // Run tests using PhantomJS
+      'PhantomJS'
     ],
 
     singleRun: true,
