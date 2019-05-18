533ec9d485864cd6d270d7b7421dd31f5906de8e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-05-18 23:50:39 +0200

    Remove deprecated dedupe plugin usage
    See:
    https://webpack.js.org/migrate/3/#dedupeplugin-has-been-removed
    
    Thanks!

--- webpack.config.js
+++ webpack.config.js
@@ -194,10 +194,6 @@ module.exports = function makeWebpackConfig() {
       // Only emit files when there are no errors
       new webpack.NoErrorsPlugin(),
 
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
-      new webpack.optimize.DedupePlugin(),
-
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
       new webpack.optimize.UglifyJsPlugin(),
